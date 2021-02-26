30d30398db91b6e583ab7f05f4869a790300bdf3
choryang <okee0815@gmail.com>
2021-02-27 00:21:56 +0900

    ADD Activity

--- app/src/main/java/com/example/roomsample/MainActivity.kt
+++ app/src/main/java/com/example/roomsample/MainActivity.kt
@@ -1,51 +1,29 @@
 package com.example.roomsample
 
+
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.lifecycleScope
-import androidx.room.Room
-import com.example.roomsample.databinding.ActivityMainBinding
-import kotlinx.coroutines.*
+
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(
-            this,
-            R.layout.activity_main
-        )
-        binding.lifecycleOwner = this
+        setContentView(R.layout.activity_main)
 
-        /*val job: Job = CoroutineScope(Dispatchers.IO).launch{
-            DatabaseCopier.copyAttachedDatabase(context = applicationContext)
+        var btn = findViewById<Button>(R.id.btn)
+
+        btn.setOnClickListener {
+            val intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
         }
 
-        runBlocking {
-            job.join()
-        }*/
-
-
-        // 추가 --
-        val db = Room.databaseBuilder(
-            applicationContext,
-            TodoDatabase::class.java, "to-do"
-        ).createFromAsset("databases/locationList.db").build()
-
-        db?.locationListDao()?.getAll()?.observe(this, { locationList ->
-            binding.resultText.text = locationList.toString()
-        })
-
-//        binding.addButton.setOnClickListener {
-//            lifecycleScope.launch(Dispatchers.IO) {
-//                db?.locationListDao()?.insert(
-//                    LocationList(0 ,title = binding.todoEdit.text.toString()))
-//            }
-//
-//            db?.locationListDao()?.getAll()?.observe(this, { todo ->
-//                binding.resultText.text = todo.toString()
-//            })
-//        }
 
 
 
